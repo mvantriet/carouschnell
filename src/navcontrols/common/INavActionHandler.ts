91a53b858606a3e4ba10ace732506fc4f3d48155
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-13 13:36:23 +0000

    Interface definition to abstract a navivgation action handler based on a nav control input action

---
+++ src/navcontrols/common/INavActionHandler.ts
@@ -0,0 +1,15 @@
+export enum NAV_DIRECTION {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+    NA
+}
+
+export interface INavActionHandler {
+    handleNavControlDirectionAction(direction:NAV_DIRECTION, offset:number): void;
+    handleNavControlEnterCurrentSelectionAction(): void;
+    handleNavControlEnterSelectionAction(row:number, column: number): void;
+    handleNavControlSelect(row:number, column: number): void;
+    handleNavControlDeselect(): void;
+}
