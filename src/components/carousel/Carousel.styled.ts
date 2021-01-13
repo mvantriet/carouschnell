c599521f327cb684ce15eaf342ee125b5e0a4117
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-13 12:46:16 +0000

    Carousel styled initial definition

---
+++ src/components/carousel/Carousel.styled.ts
@@ -0,0 +1,7 @@
+import * as styled from "styled-components";
+
+export type CarouselStyledProps = {};
+
+const CarouselStyled = styled.default.div<CarouselStyledProps>`
+`;
+export default CarouselStyled;
