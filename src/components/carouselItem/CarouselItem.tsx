4331ebf4902c7367b1c2666b48cb7431d20dbeb2
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-13 22:14:50 +0000

    Support implementors of INavItemActionHandler for mouse and click events

--- src/components/carouselItem/CarouselItem.tsx
+++ src/components/carouselItem/CarouselItem.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import {CSSTransition} from 'react-transition-group';
 import { CarouselItemStyled, CarouselItemSelectedOverlayStyled } from "./CarouselItem.styled";
 import { CarouselItemConfig, CarouselItemStyleConfig} from "../../config/CarouselConfig";
+import {INavItemActionHandler} from "../../navcontrols/common/INavItemActionHandler";
+
 export type CarouselItemProps = {
     config: CarouselItemConfig;
     selected: boolean,
@@ -9,6 +11,7 @@ export type CarouselItemProps = {
     inOverrun: boolean,
     xNavOffset: number,
     yNavOffset: number,
+    navActionHandlers: Array<INavItemActionHandler>,
     style: CarouselItemStyleConfig
 };
 
@@ -26,11 +29,23 @@ export const CarouselItem: React.FunctionComponent<CarouselItemProps> = (props: 
     in={false} 
     timeout={0} 
     className="item">
+      {props.navActionHandlers.length > 0 ? 
+        <div
+        onMouseEnter={(event:any) => props.navActionHandlers.forEach((handler:INavItemActionHandler) => handler.handleItemHoverEnter(event, props.yNavOffset, props.xNavOffset))}
+        onMouseLeave={(event:any) => props.navActionHandlers.forEach((handler:INavItemActionHandler) => handler.handleItemHoverExit(event, props.yNavOffset, props.xNavOffset))}
+        onMouseMove={(event:any) => props.navActionHandlers.forEach((handler:INavItemActionHandler) => handler.handleItemHover(event, props.yNavOffset, props.xNavOffset))}
+        onClick={(event:any) => props.navActionHandlers.forEach((handler:INavItemActionHandler) => handler.handleItemOnClick(event, props.yNavOffset, props.xNavOffset))}
+        className="item">
+          <img src={props.config.thumbnail} alt={props.config.caption}></img>
+          {props.selected && <CarouselItemSelectedOverlayStyled style={props.style}><span>{props.config.caption}</span></CarouselItemSelectedOverlayStyled>}
+        </div>
+      : 
         <div
         className="item">
           <img src={props.config.thumbnail} alt={props.config.caption}></img>
           {props.selected && <CarouselItemSelectedOverlayStyled style={props.style}><span>{props.config.caption}</span></CarouselItemSelectedOverlayStyled>}
         </div>
+      }
   </CSSTransition>
 </CarouselItemStyled>
 };
