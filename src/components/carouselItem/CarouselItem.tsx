c898822ba5e5acffdd1feb32deda85298eb03fc9
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-13 13:06:49 +0000

    CarouselItem changes according to styled component updates

--- src/components/carouselItem/CarouselItem.tsx
+++ src/components/carouselItem/CarouselItem.tsx
@@ -1,12 +1,37 @@
 import * as React from "react";
+import {CSSTransition} from 'react-transition-group';
 import { CarouselItemStyled, CarouselItemSelectedOverlayStyled } from "./CarouselItem.styled";
-import { CarouselItemConfig } from "../../config/CarouselConfig";
+import { CarouselItemConfig, CarouselItemStyleConfig} from "../../config/CarouselConfig";
 export type CarouselItemProps = {
     config: CarouselItemConfig;
+    selected: boolean,
+    inView: boolean,
+    inOverrun: boolean,
+    xNavOffset: number,
+    yNavOffset: number,
+    style: CarouselItemStyleConfig
 };
 
-export const CarouselItem: React.FunctionComponent<CarouselItemProps> = (
-    props: CarouselItemProps
-) => {
-    return <CarouselItemStyled />;
+export const CarouselItem: React.FunctionComponent<CarouselItemProps> = (props: CarouselItemProps) => {
+
+    return <CarouselItemStyled 
+    className={props.selected ? "selected" : ""} 
+    inView={props.inView} 
+    inOverrun={props.inOverrun} 
+    thumbnailUrl={props.config.thumbnail} 
+    selected={props.selected} 
+    xNavOffset={props.xNavOffset} 
+    yNavOffset={props.yNavOffset}
+    style={props.style}>
+  <CSSTransition 
+    in={false} 
+    timeout={0} 
+    className="item">
+        <div
+        className="item">
+          <img src={props.config.thumbnail} alt={props.config.caption}></img>
+          {props.selected && <CarouselItemSelectedOverlayStyled style={props.style}><span>{props.config.caption}</span></CarouselItemSelectedOverlayStyled>}
+        </div>
+  </CSSTransition>
+</CarouselItemStyled>
 };
