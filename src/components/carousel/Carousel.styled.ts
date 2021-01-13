f0c611b4485c303cebdc03ee0a1dd721681fae60
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-13 13:14:37 +0000

    Styling for inner grid, i.e. active items only, no overrun

--- src/components/carousel/Carousel.styled.ts
+++ src/components/carousel/Carousel.styled.ts
@@ -40,5 +40,18 @@ const CarouselStyled = styled.default.div<CarouselStyledProps>`
         border-radius: ${props => props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.BIGSCREEN].borderRadiusSizeOverrun}px;
         border: ${props => props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.BIGSCREEN].borderSizeOverrun}px solid ${props => props.style.theme.borderColorOverrun};
     }
+
+    .grid-inner {
+        background-color: ${props => props.style.theme.backgroundColorActive};
+        border-radius: ${props => props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL].borderRadiusSizeActive}px;
+        border: ${props => props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL].borderSizeActive}px solid ${props => props.style.theme.borderColorActive};
+        margin-top: ${props => StyleUtils.calcOverrunYSpace(props.style.itemStyleConfig, props.display, CAROUSEL_STYLE_MEDIA_TYPE.SMALL) - 
+                                                            props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL].activeExtraPaddingTop}px;
+        margin-left: ${props => props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL].activeExtraPaddingX * -1}px;
+        width: ${props => StyleUtils.calcActiveXSpace(props.style.itemStyleConfig, props.display, CAROUSEL_STYLE_MEDIA_TYPE.SMALL) + 
+                                                      props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL].activeExtraPaddingX}px;
+        height: ${props => StyleUtils.calcActiveYSpace(props.style.itemStyleConfig, props.display, CAROUSEL_STYLE_MEDIA_TYPE.SMALL) + 
+                                                       props.style.mediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL].activeExtraPaddingBottom}px;
+    }
 `;
 export default CarouselStyled;
