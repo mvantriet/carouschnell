5458c20e81747d78a77f74c7ce5c681ab4588236
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-13 13:26:55 +0000

    Carousel rendering based on item configs and CarouselItem interface

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import CarouselStyled from "./Carousel.styled";
 import { CarouselItem } from "../carouselItem/CarouselItem";
-import { CarouselConfig } from "../../config/CarouselConfig";
+import {CarouselConfig, CarouselItemConfig, CarouselDisplayConfig, 
+    CarouselStyleConfig, CarouselRowConfig} from "../../config/CarouselConfig";
+import {GridUtils} from '../../utils/GridUtils';
 
 export type ItemState = {
+    config: CarouselItemConfig,
     xOffset: number;
     yOffset: number;
 };
@@ -19,9 +22,18 @@ export type CarouselProps = {
 };
 
 export class Grid extends React.Component<CarouselProps, CarouselState> {
+
+    private displayConfig: CarouselDisplayConfig;
+    private enable2dNav: boolean;
+    private styleConfig: CarouselStyleConfig;
+  
+
     constructor(props: CarouselProps) {
         super(props);
-
+        this.enable2dNav = props.config.navControls.enable2dNav;
+        this.displayConfig = props.config.displayConfig;
+        this.styleConfig = props.config.styleConfig;
+    
         this.state = {
             activeDisplayColumn: props.config.displayConfig.initialDisplayColumn,
             activeDisplayRow: props.config.displayConfig.initialDisplayRow,
@@ -31,9 +43,9 @@ export class Grid extends React.Component<CarouselProps, CarouselState> {
 
     private initialiseItemStates(props: CarouselProps): Array<ItemState> {
         let itemStates: Array<ItemState> = [];
-        props.config.rows.forEach((row, rowIndex) => {
+        props.config.rows.forEach((row: CarouselRowConfig, rowIndex: number) => {
             itemStates = itemStates.concat(
-                row.items.map((item, itemIndex) => {
+                row.items.map((item: CarouselItemConfig, itemIndex: number) => {
                     return {
                         config: item,
                         xOffset: row.initialColumn - itemIndex,
@@ -48,6 +60,25 @@ export class Grid extends React.Component<CarouselProps, CarouselState> {
     componentDidMount() {}
 
     render() {
-        return <CarouselStyled />;
+    return <CarouselStyled
+        style={this.styleConfig}
+        display={this.displayConfig}
+      >
+      <div className="grid">
+      {
+            this.state.itemStates.map((itemState:ItemState, index:number) => {
+              return <CarouselItem key={index.toString()}
+                style={this.styleConfig.itemStyleConfig}
+                xNavOffset={itemState.xOffset} 
+                yNavOffset={itemState.yOffset}
+                inView={GridUtils.isItemInView(itemState.yOffset, itemState.xOffset, this.displayConfig)}
+                inOverrun={GridUtils.isItemInOverrun(itemState, this.displayConfig).result}
+                selected={GridUtils.isItemSelected(itemState, this.state.activeDisplayRow, this.state.activeDisplayColumn)} 
+                config={itemState.config} />
+            })
+          }
+      <div id="grid-inner" className="grid-inner"/>
+    </div>
+  </CarouselStyled>
     }
 }
