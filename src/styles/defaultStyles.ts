a2b389ae851c722133fbe8cc6aff295c0eb46003
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-27 14:30:29 +0000

    Define a lightstyle which can easily be used by developers

--- src/styles/defaultStyles.ts
+++ src/styles/defaultStyles.ts
@@ -98,6 +98,7 @@ defaultStyleItemMediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL] = {
         },
     },
     selectionStyleConfig: {
+        relativeHeight: 20,
         bottomRadiusSize: 20,
         fontSize: 10,
         topBorderShadowSize: 20,
@@ -123,6 +124,7 @@ defaultStyleItemMediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.BIGSCREEN] = {
         },
     },
     selectionStyleConfig: {
+        relativeHeight: 20,
         bottomRadiusSize: 20,
         fontSize: 16,
         topBorderShadowSize: 20,
@@ -148,6 +150,7 @@ defaultStyleItemMediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.DESKTOP] = {
         },
     },
     selectionStyleConfig: {
+        relativeHeight: 20, 
         bottomRadiusSize: 20,
         fontSize: 16,
         topBorderShadowSize: 20,
@@ -187,10 +190,146 @@ export const darkStyle: CarouselStyleConfig = {
 };
 
 /**
- * TODO: Light style
+ * Light style
  */
+
+
+const lightStyleItemMediaTypes: CarouselItemStyleConfigMediaTypes = [];
+lightStyleItemMediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.SMALL] = {
+    itemSelectionFontSize: 14,
+    itemBorderSize: 1,
+    itemSelectedBorderSize: 4,
+    borderRadius: 15,
+    itemSize: {
+        size: {
+            x: 200,
+            y: 150,
+        },
+        margin: {
+            x: 20,
+            y: 20,
+        },
+    },
+    itemSizeOverrun: {
+        size: {
+            x: 136,
+            y: 102,
+        },
+        margin: {
+            x: 10,
+            y: 10,
+        },
+    },
+    selectionStyleConfig: {
+        relativeHeight: 10, 
+        bottomRadiusSize: 20,
+        fontSize: 14,
+        topBorderShadowSize: 20,
+    },
+    overrunDirectionDisplay: {
+        size: 17
+    }
+};
+lightStyleItemMediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.BIGSCREEN] = {
+    itemSelectionFontSize: 14,
+    itemBorderSize: 2,
+    itemSelectedBorderSize: 6,
+    borderRadius: 25,
+    itemSize: {
+        size: {
+            x: 400,
+            y: 300,
+        },
+        margin: {
+            x: 40,
+            y: 40,
+        },
+    },
+    itemSizeOverrun: {
+        size: {
+            x: 300,
+            y: 200,
+        },
+        margin: {
+            x: 20,
+            y: 20,
+        },
+    },
+    selectionStyleConfig: {
+        relativeHeight: 10, 
+        bottomRadiusSize: 20,
+        fontSize: 20,
+        topBorderShadowSize: 20,
+    },
+    overrunDirectionDisplay: {
+        size: 35
+    }
+};
+lightStyleItemMediaTypes[CAROUSEL_STYLE_MEDIA_TYPE.DESKTOP] = {
+    itemSelectionFontSize: 14,
+    itemBorderSize: 2,
+    itemSelectedBorderSize: 6,
+    borderRadius: 25,
+    itemSize: {
+        size: {
+            x: 400,
+            y: 300,
+        },
+        margin: {
+            x: 40,
+            y: 40,
+        },
+    },
+    itemSizeOverrun: {
+        size: {
+            x: 300,
+            y: 200,
+        },
+        margin: {
+            x: 20,
+            y: 20,
+        },
+    },
+    selectionStyleConfig: {
+        relativeHeight: 10,
+        bottomRadiusSize: 20,
+        fontSize: 20,
+        topBorderShadowSize: 20,
+    },
+    overrunDirectionDisplay: {
+        size: 35
+    }
+};
+
+
+export const lightStyle: CarouselStyleConfig = {
+    itemStyleConfig: {
+        theme: {
+            itemBorderColor: "white",
+            itemBackgroundColor: "transparent",
+            itemSelectedBorderColor: "#6699ff",
+            itemBorderShadowColor: "black",
+            itemSelectionFontColor: "white",
+            itemSelectionBackgroundColor: "#6699ff",
+            itemSelectionForegroundColor: "white",
+            itemInOverrunOpacity: 60,
+            initialOpacityOverrunDirectionDisplay: 0.7
+        },
+        mediaTypes: lightStyleItemMediaTypes,
+    },
+    theme: {
+        backgroundColorActive: "white",
+        borderColorActive: "#6699ff",
+        backgroundColorOverrun: "white",
+        borderColorOverrun: "#6699ff",
+        fontFamily: "muli, sans-serif",
+        fontWeight: 300,
+    },
+    mediaTypes: defaultStyleMediaTypes,
+};
 
 export const defaultStyles = {
     darkStyle: darkStyle,
+    lightStyle: lightStyle
     // Add new default styles here
 };
