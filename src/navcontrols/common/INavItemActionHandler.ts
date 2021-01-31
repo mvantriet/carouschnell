c6ab457d4b3b4fcb170b48a226a22210d25b934f
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-01-31 19:44:00 +0000

    Define separate interfaces for mouse and touch nav item actions handlers

--- src/navcontrols/common/INavItemActionHandler.ts
+++ src/navcontrols/common/INavItemActionHandler.ts
@@ -1,9 +1,18 @@
-import { MouseEvent } from "react";
+import { MouseEvent, TouchEvent } from "react";
 
 export interface INavItemActionHandler {
     init(): void;
+}
+
+export interface IMouseNavItemActionHandler extends INavItemActionHandler {
     handleItemHoverEnter(event: MouseEvent, row: number, column: number): void;
     handleItemHoverExit(event: MouseEvent, row: number, column: number): void;
     handleItemHover(event: MouseEvent, row: number, column: number): void;
     handleItemOnClick(event: MouseEvent, row: number, column: number): void;
+}
+
+export interface ITouchNavItemActionHandler extends INavItemActionHandler {
+    handleItemOnTouchStart(event: TouchEvent, row: number, column: number): void;
+    handleItemOnTouchMove(event: TouchEvent, row: number, column: number): void;
+    handleItemOnTouchEnd(event: TouchEvent, row: number, column: number): void;
 }
