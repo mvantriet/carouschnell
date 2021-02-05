b4924ad4700aaad96cab2560523436fa22f9666f
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-05 22:19:52 +0000

    Export nav direction enumeration to be used by developers when using custom controls

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
 export { Carousel } from "./components/carousel/Carousel";
 export { defaultStyles } from "../src/styles/defaultStyles";
 export { DEVICE_NAV_KEYCODES_DEFAULT } from "./navcontrols/keyboard/KeyboardNavController";
+export { NAV_DIRECTION } from "./navcontrols/common/INavActionHandler";
