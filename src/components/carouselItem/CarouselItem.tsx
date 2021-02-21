8ce4ba1dc56fb0663e8341d445969170ecad0452
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-21 15:48:50 +0000

    Define new property to facilitate lazy loading

--- src/components/carouselItem/CarouselItem.tsx
+++ src/components/carouselItem/CarouselItem.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
 import { CSSTransition } from "react-transition-group";
-import { CarouselItemStyled, CarouselItemSelectedOverlayStyled, CarouselItemOverrunOverlayStyled } from "./CarouselItem.styled";
+import {
+    CarouselItemStyled,
+    CarouselItemSelectedOverlayStyled,
+    CarouselItemOverrunOverlayStyled,
+} from "./CarouselItem.styled";
 import { CarouselItemConfig, CarouselItemStyleConfig } from "../../config/CarouselConfig";
-import { IMouseNavItemActionHandler, ITouchNavItemActionHandler} from "../../navcontrols/common/INavItemActionHandler";
+import {
+    IMouseNavItemActionHandler,
+    ITouchNavItemActionHandler,
+} from "../../navcontrols/common/INavItemActionHandler";
 import { NavDirectionResult } from "../../utils/CarouselUtils";
 
 export type CarouselItemProps = {
@@ -13,12 +20,15 @@ export type CarouselItemProps = {
     overrunDirection: NavDirectionResult;
     xNavOffset: number;
     yNavOffset: number;
+    loadSrc: boolean;
     mouseNavActionHandlers: Array<IMouseNavItemActionHandler>;
     touchNavActionHandlers: Array<ITouchNavItemActionHandler>;
     style: CarouselItemStyleConfig;
 };
 
-export const CarouselItem: React.FunctionComponent<CarouselItemProps> = (props: CarouselItemProps) => {
+export const CarouselItem: React.FunctionComponent<CarouselItemProps> = (
+    props: CarouselItemProps
+) => {
     return (
         <CarouselItemStyled
             className={props.selected ? "selected" : ""}
@@ -31,66 +41,95 @@ export const CarouselItem: React.FunctionComponent<CarouselItemProps> = (props: 
             style={props.style}
         >
             <CSSTransition in={false} timeout={0} className="item">
-                    <div
-                        onMouseEnter={(event: any) =>
-                            props.mouseNavActionHandlers.forEach((handler: IMouseNavItemActionHandler) =>
+                <div
+                    onMouseEnter={(event: any) =>
+                        props.mouseNavActionHandlers.forEach(
+                            (handler: IMouseNavItemActionHandler) =>
                                 handler.handleItemHoverEnter(
                                     event,
                                     props.yNavOffset,
                                     props.xNavOffset
                                 )
-                            )
-                        }
-                        onMouseLeave={(event: any) =>
-                            props.mouseNavActionHandlers.forEach((handler: IMouseNavItemActionHandler) =>
+                        )
+                    }
+                    onMouseLeave={(event: any) =>
+                        props.mouseNavActionHandlers.forEach(
+                            (handler: IMouseNavItemActionHandler) =>
                                 handler.handleItemHoverExit(
                                     event,
                                     props.yNavOffset,
                                     props.xNavOffset
                                 )
-                            )
-                        }
-                        onMouseMove={(event: any) =>
-                            props.mouseNavActionHandlers.forEach((handler: IMouseNavItemActionHandler) =>
+                        )
+                    }
+                    onMouseMove={(event: any) =>
+                        props.mouseNavActionHandlers.forEach(
+                            (handler: IMouseNavItemActionHandler) =>
                                 handler.handleItemHover(event, props.yNavOffset, props.xNavOffset)
-                            )
-                        }
-                        onTouchStart={(event: any) =>
-                            props.touchNavActionHandlers.forEach((handler: ITouchNavItemActionHandler) =>
-                                handler.handleItemOnTouchStart(event, props.yNavOffset, props.xNavOffset)
-                            )
-                        }
-                        onTouchMove={(event: any) =>
-                            props.touchNavActionHandlers.forEach((handler: ITouchNavItemActionHandler) =>
-                                handler.handleItemOnTouchMove(event, props.yNavOffset, props.xNavOffset)
-                            )
-                        }
-                        onTouchEnd={(event: any) =>
-                            props.touchNavActionHandlers.forEach((handler: ITouchNavItemActionHandler) =>
-                                handler.handleItemOnTouchEnd(event, props.yNavOffset, props.xNavOffset)
-                            )
-                        }
-                        onClick={(event: any) =>
-                            props.mouseNavActionHandlers.forEach((handler: IMouseNavItemActionHandler) =>
+                        )
+                    }
+                    onTouchStart={(event: any) =>
+                        props.touchNavActionHandlers.forEach(
+                            (handler: ITouchNavItemActionHandler) =>
+                                handler.handleItemOnTouchStart(
+                                    event,
+                                    props.yNavOffset,
+                                    props.xNavOffset
+                                )
+                        )
+                    }
+                    onTouchMove={(event: any) =>
+                        props.touchNavActionHandlers.forEach(
+                            (handler: ITouchNavItemActionHandler) =>
+                                handler.handleItemOnTouchMove(
+                                    event,
+                                    props.yNavOffset,
+                                    props.xNavOffset
+                                )
+                        )
+                    }
+                    onTouchEnd={(event: any) =>
+                        props.touchNavActionHandlers.forEach(
+                            (handler: ITouchNavItemActionHandler) =>
+                                handler.handleItemOnTouchEnd(
+                                    event,
+                                    props.yNavOffset,
+                                    props.xNavOffset
+                                )
+                        )
+                    }
+                    onClick={(event: any) =>
+                        props.mouseNavActionHandlers.forEach(
+                            (handler: IMouseNavItemActionHandler) =>
                                 handler.handleItemOnClick(event, props.yNavOffset, props.xNavOffset)
-                            )
-                        }
-                        className="item"
-                    >
+                        )
+                    }
+                    className="item"
+                >
+                    {props.loadSrc ? (
                         <img src={props.config.thumbnail} alt={props.config.caption}></img>
-                        {props.selected && (
-                            <CarouselItemSelectedOverlayStyled style={props.style}>
-                                <span>{props.config.caption}</span>
-                            </CarouselItemSelectedOverlayStyled>
+                    ) : (
+                        <img alt={props.config.caption}></img>
+                    )}
+                    {props.selected && (
+                        <CarouselItemSelectedOverlayStyled style={props.style}>
+                            <span>{props.config.caption}</span>
+                        </CarouselItemSelectedOverlayStyled>
+                    )}
+                    <CSSTransition
+                        in={props.inOverrun && props.overrunDirection.result}
+                        timeout={300}
+                        classNames="overrundir"
+                        unmountOnExit
+                    >
+                        {() => (
+                            <CarouselItemOverrunOverlayStyled
+                                style={props.style}
+                                direction={props.overrunDirection.direction}
+                            />
                         )}
-                            <CSSTransition 
-                                in={props.inOverrun && props.overrunDirection.result} 
-                                timeout={300} 
-                                classNames="overrundir"
-                                unmountOnExit>
-                                {() => <CarouselItemOverrunOverlayStyled style={props.style} direction={props.overrunDirection.direction}/>}
-                            </CSSTransition>
-                    </div>
+                    </CSSTransition>
+                </div>
             </CSSTransition>
         </CarouselItemStyled>
     );
