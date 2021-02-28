8f85eed3ae5d001bdf7c6ffd83498fe415e0fc60
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-02-28 23:57:50 +0000

    Implement utility functions for calculating the offsets for row labels

--- src/utils/StyleUtils.ts
+++ src/utils/StyleUtils.ts
@@ -2,6 +2,7 @@ import {
     CarouselItemStyleConfig,
     CarouselDisplayConfig,
     CAROUSEL_STYLE_MEDIA_TYPE,
+    CarouselRowLabelConfig,
 } from "../config/CarouselConfig";
 
 export class StyleUtils {
@@ -18,11 +19,12 @@ export class StyleUtils {
 
     static calcYSpace(
         itemStyleConfig: CarouselItemStyleConfig,
+        rowLabelStyleConfig: CarouselRowLabelConfig | undefined,
         displayConfig: CarouselDisplayConfig,
         mediaType: CAROUSEL_STYLE_MEDIA_TYPE
     ): number {
         return (
-            this.calcActiveYSpace(itemStyleConfig, displayConfig, mediaType) +
+            this.calcActiveYSpace(itemStyleConfig, rowLabelStyleConfig, displayConfig, mediaType) +
             this.calcOverrunYSpace(itemStyleConfig, displayConfig, mediaType) * 2
         );
     }
@@ -65,13 +67,23 @@ export class StyleUtils {
 
     static calcActiveYSpace(
         itemStyleConfig: CarouselItemStyleConfig,
+        rowLabelStyleConfig: CarouselRowLabelConfig | undefined,
         displayConfig: CarouselDisplayConfig,
         mediaType: CAROUSEL_STYLE_MEDIA_TYPE
     ): number {
         return (
             (itemStyleConfig.mediaTypes[mediaType].itemSize.size.y +
-                itemStyleConfig.mediaTypes[mediaType].itemSize.margin.y) *
+                itemStyleConfig.mediaTypes[mediaType].itemSize.margin.y + this.calcRowLabelYSpace(rowLabelStyleConfig, mediaType)) *
             (displayConfig.rowEnd - (displayConfig.rowStart - 1))
         );
+    }
+
+    static calcRowLabelYSpace(rowLabelStyleConfig: CarouselRowLabelConfig | undefined, mediaType: CAROUSEL_STYLE_MEDIA_TYPE): number {
+        return (rowLabelStyleConfig ? rowLabelStyleConfig.mediaTypes[mediaType].fontSize + rowLabelStyleConfig.mediaTypes[mediaType].paddingBottom +
+            rowLabelStyleConfig.mediaTypes[mediaType].paddingTop : 0);
+    }
+
+    static calcItemYSpace(itemStyleConfig: CarouselItemStyleConfig, mediaType: CAROUSEL_STYLE_MEDIA_TYPE): number {
+        return itemStyleConfig.mediaTypes[mediaType].itemSize.size.y + itemStyleConfig.mediaTypes[mediaType].itemSize.margin.y;
     }
 }
