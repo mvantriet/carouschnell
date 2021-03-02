964e8544e0d5f9ffd89eaee32633603bcbd7cff7
Marnix van 't Riet <marnix.vantriet@bbc.co.uk>
2021-03-02 11:28:56 +0000

    Export carousel media types for developer customizations

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { Carousel } from "./components/carousel/Carousel";
 export { defaultStyles } from "./styles/defaultStyles";
 export { DEVICE_NAV_KEYCODES_DEFAULT } from "./navcontrols/keyboard/KeyboardNavController";
 export { NAV_DIRECTION } from "./navcontrols/common/INavActionHandler";
+export { CAROUSEL_STYLE_MEDIA_TYPE } from "./config/CarouselConfig";
